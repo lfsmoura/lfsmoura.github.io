ac26864dff3399d0dcb57614a33abcb9f17d54c5
Leonardo Moura <leofsmoura@gmail.com>
2016-09-26 09:19:11 -0300

    Add animated header

--- js/main.js
+++ js/main.js
@@ -14,3 +14,29 @@ if ('serviceWorker' in navigator) {
     console.log('Registration failed with ' + error);
   });
 };
+
+function delayedThrottle(fun, time) {
+  time = time || 250;
+  var lastCallTime = 0;
+  return function() {
+    var now = new Date().getTime();
+    if (now - lastCallTime > time) {
+      lastCallTime = now;
+      setTimeout(fun, time);
+    }
+  };
+}
+
+$(window).scroll(
+    {
+        previousTop: 0
+    },
+    delayedThrottle(function () {
+    var currentTop = $(window).scrollTop();
+    if (currentTop < this.previousTop) {
+        $(".site-header").animate({ opacity: 1 }, 250);
+    } else if (currentTop > 200 && currentTop > this.previousTop) {
+        $(".site-header").animate({ opacity: 0 }, 250);
+    }
+    this.previousTop = currentTop;
+}));
